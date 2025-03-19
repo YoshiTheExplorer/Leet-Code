3b3440b51da943b5ba53d5a2c97b75f4274f493b
Colin Yamada <colin.yamada@gmail.com>
2025-03-18 23:53:34 -0500

    Update Solution.java

--- Valid_Palindrome/src/Solution.java
+++ Valid_Palindrome/src/Solution.java
@@ -1,3 +1,20 @@
-public class Solution {
-
+class Solution {
+    public boolean isPalindrome(String s) {
+        s = s.toLowerCase().replaceAll("\\s+","");
+        char[] sentence = s.toCharArray();
+        int endP = sentence.length-1;
+        int c;
+        for(int i = 0; i < sentence.length/2; i++){
+            c = (int) sentence[i];
+            if(c < 0 && c > 127){
+                --endP;
+            }
+            
+            if(sentence[i] != sentence[endP]){
+                return false;
+            }
+            --endP;
+        }
+        return true;
+    }
 }
